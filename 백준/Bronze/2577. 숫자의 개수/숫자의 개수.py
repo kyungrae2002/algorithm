3aea6c8ea14615219baf95dcbab450d8b6475803
kyungrae2002 <148605113+kyungrae2002@users.noreply.github.com>
2026-03-21 17:07:52 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,5 +1,11 @@
-A = int(input())
-B = int(input())
-C = int(input())
-for i in range(10):
-    print(str(A*B*C).count(str(i)))
+mul =1 #곱하기 결과
+for i in range(3):
+    a = int(input()) # 매번 받고
+    mul *= a ## 매번 곱하기
+nums = [0]*10 ## 빈 배열 만들기
+while(mul >= 1): ## 곱셈 결과가 1이상일때
+    if(int(mul%10) >= 0): 
+        nums[int(mul%10)] += 1
+    mul/=10
+for i in nums:
+    print(i)
