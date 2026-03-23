2a750a9888d145d6c658739beeba65b09384071e
kyungrae2002 <148605113+kyungrae2002@users.noreply.github.com>
2026-03-23 19:08:35 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,33 +1,35 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main(void) {
-	int a;
-	cin >> a; // 인풋 cin 으로 받기
-	vector<int> vec{}; // 빈 벡터 생성 (인풋 받기)
-	for (int i = 0; i < a; i++) { 
-		int b; 
-		cin >> b; // 여러개의 인풋 받기 
-		vec.push_back(b); // 받아서 생성한 벡터에 추가
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N = 0;
+	cin >> N; // 원소 받기
+	//동적 할당으로 배열 정의
+	int* list = new int[N];
+	for (int i = 0; i < N; i++) {
+		//원소 받기
+		int m = 0;
+		cin >> m;
+		list[i] = m;
 	}
-	int num = 0;
-	int target;
+	// 인풋 숫자 초기화
+	int target = 0;
+	// count 횟수 초기화
+	int count = 0;
 	cin >> target;
-	int *arr = new int[target];
-	// 시간을 보고 O(N) 안에 해결해야 하므로, 배열 생성 -> 따로 저장
-	// 동적할당으로 따로 저장하려고 함 (원소 찾기 배열)
-	for (int i = 0; i < target; i++) {
-		arr[i] = 0; // 동적 할당 배열 0초기화
+	// 인풋 받기
+	// 동적 할당으로 정의 (값을 저장할 배열 (배열 원소 찾기 O(1)), 초기화
+	int* search = new int[target+1]();
+	for (int i = 0; i < N; i++) {
+		if (target < list[i]) { continue; }
+		// list[i] 값이 크다면 인덱싱 오류, 패스
+		if (search[target - list[i]] >= 1) { count++; }
+		// 타겟 - 원소로 인덱싱하여 그 값을 찾고, 그 값이 앞서 나왔다면 count 증가
+		search[list[i]]++;
+		// 나왔다면 원소 값 1증가
 	}
-	for (int i = 0; i < vec.size(); i++) {
-		if(target-vec[i]-1 >=0){ //segmentation error 핸들링
-			if (arr[target - vec[i] - 1] != 0) {
-				num++; // 배열의 원소를 찾는 것은 O(1) 안에 찾으므로
-			}
-		}
-		if(vec[i]-1 < target) // segmentation error 핸들링
-			arr[vec[i] - 1]++; // 앞선 원소 찾기 배열에서 해당 인덱스에 해당하는 값 1증가
-	}
-	delete[] arr; // 메모리 해제
-	cout << num << '\n';
+	delete[] list; // 동적 할당 메모리 날리기
+	delete[] search;
+	cout << count << '\n';
 }
