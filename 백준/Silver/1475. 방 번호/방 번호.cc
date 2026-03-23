e3557ee6c751bc4ffc6981c0963e527a437b3427
kyungrae2002 <148605113+kyungrae2002@users.noreply.github.com>
2026-03-23 18:45:04 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,24 +1,25 @@
 #include <iostream>
 using namespace std;
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
 	int N;
 	cin >> N;
-	int arr[10]{};
-	while (N >= 1) {
-		arr[N % 10]+=1;
+	int store[10]{}; // 저장할 빈 배열
+	do { // N 자리에 0이 들어올 수 있으니
+		// 6, 9는 하나로 처리
+		if (N % 10 == 6 || N % 10 == 9) {
+			store[6]++;
+		}
+		else {
+			store[N % 10]++;
+		}
 		N /= 10;
-	}
-	if (arr[6] - arr[9] >= 2) {
-		arr[6] = arr[9] + (arr[6] - arr[9] + 1) / 2;
-	}
-	else if (arr[6] - arr[9] <= -2) {
-		arr[9] = arr[6] + (arr[9] - arr[6] + 1 ) / 2;
-	}
+	} while (N);
+	// 짝수 개든 홀수 개든 알아서 처리할 수 있도록
+	store[6] = (store[6] + 1) / 2;
 	int max = 0;
-	for (int el : arr) {
-		max = (max > el) ? max : el;
+	// 최대값 찾기 알고리즘
+	for (int i = 0; i < 10; i++) {
+		max = (max > store[i]) ? max : store[i];
 	}
 	cout << max;
 }
