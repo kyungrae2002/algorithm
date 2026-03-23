354502f316139393f4dd62b485d1c3ece4eb20f8
kyungrae2002 <148605113+kyungrae2002@users.noreply.github.com>
2026-03-23 19:40:53 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.cc
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -1,26 +1,20 @@
-#include <string.h>
 #include <iostream>
 using namespace std;
-
-int main(void){
-	ios::sync_with_stdio(0);
+int main(void) {
 	cin.tie(0);
-	int N, K;
-	cin >> N >> K;
-	int w[7] = { 0, }; // 여자 배열
-	int m[7] = { 0, }; // 남자 배열
+	ios::sync_with_stdio(0);
+	int N = 0, K = 0;
+	cin >> N >> K; // 인풋 받기
+	int* rooms = new int[13](); // 배열 초기화
+	int room_num = 0; // 방개수 정의
 	for (int i = 0; i < N; i++) {
-		int s, g;
-		cin >> s >> g;
-		if (s) { m[g]++; }
-		else { w[g]++; }
+		int gender = 0, grade = 0;
+		cin >> gender >> grade; // 성별, 학년 받기
+		rooms[gender * 6 + grade]++; // 성별, 학년을 이용한 배열
 	}
-	int cnt = 0;
-	for (int i = 1; i < 7; i++) {
-		if (m[i] % K == 0) { cnt += m[i] / K; }
-		if (m[i] % K != 0) { cnt += int(m[i] / K) +1; }
-		if (w[i] % K == 0) { cnt += w[i] / K; }
-		if (w[i] % K != 0) { cnt += int(w[i] / K) + 1; }
-	}
-	cout << cnt;
+	for (int i = 0; i < 13; i++) {
+		room_num += (rooms[i] + K - 1) / K;
+	}// 일반적인 상황을 처리하기 위해 k-1 더하고 k로 나누기
+	cout << room_num; // 방개수 출력
+	delete[] rooms; //메모리 초기화
 }
