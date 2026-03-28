8e9a74d3132bf32a04933a56191ec19fd58491eb
kyungrae2002 <148605113+kyungrae2002@users.noreply.github.com>
2026-03-29 00:23:28 +0900

    [Silver IV] Title: 카드2, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,27 +1,32 @@
+/*
+* 위에 있는 카드 바닥으로 bool end = true; 반복문 1차 순환
+* true 인 상태에서 버리기 bool end = false;
+* 이거 반복하면 될듯
+* 그리고 만약에 끝=시작 이면 종료 -> while 문으로 하면 될듯
+*/
 #include <iostream>
-#include <vector>
 using namespace std;
-const int MAX = 1000000;
-int Q[MAX]{};
-int head, tail = 0;
+int arr[1000000]{};
 int main(void) {
-	cin.tie(0);
-	ios::sync_with_stdio(0);
 	int N;
 	cin >> N;
-	int check = 0;
+	int start = 1;
+	int bottom = N;
 	for (int i = 0; i < N; i++) {
-		Q[i] = i + 1;
-		tail++;
-	} // 숫자 채우기
-	while (tail != head+1) {
-		if (check % 2 == 0) {
-			head++;
-		} // 버리기
-		else {
-			Q[++tail-1] = Q[head++];
-		} // 이동 후 버리기
-		check++;
+		arr[i+1] = i + 1;
 	}
-	cout << Q[tail-1];
+	bool end = true;
+	while (bottom != start) {
+		if (end) {
+			end = false;
+			start += 1;
+		}
+		else {
+			end = true;
+			arr[bottom + 1] = arr[start];
+			start++;
+			bottom++;
+		}
+	}
+	cout << arr[bottom];
 }
